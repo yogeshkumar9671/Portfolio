e3b0da55eaf149015a04a9e7a38988b745f776d7
yogeshkumar9671 <97506050+yogeshkumar9671@users.noreply.github.com>
2025-05-13 17:44:27 +0530

    Add files via upload
    Signed-off-by: yogeshkumar9671 <97506050+yogeshkumar9671@users.noreply.github.com>

--- script.js
+++ script.js
@@ -477,81 +477,27 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // <!-- ======= MAGIC-CURSOR START ======= -->
 // <!-- ======= MAGIC-CURSOR START ======= -->
 // <!-- ======= MAGIC-CURSOR START ======= -->
-// var cursor = document.querySelector('#circle-cursor');
-// var gElems = document.getElementsByClassName('cursor-hover-element');
-// document.onmousemove = handleMouseMove;
-// var isInElement = false;
-// function handleMouseMove(event) {
-//     var dot, eventDoc, doc, body, pageX, pageY;
-//     event = event || window.event;
-//     if (event.pageX == null && event.clientX != null) {
-//         eventDoc = (event.target && event.target.ownerDocument) || document;
-//         doc = eventDoc.documentElement;
-//         body = eventDoc.body;
+const rings = document.querySelectorAll('.ring');
+const cursor = document.getElementById('cursor');
+const links = document.querySelectorAll('a');
 
-//         event.pageX = event.clientX +
-//             (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
-//             (doc && doc.clientLeft || body && body.clientLeft || 0);
-//         event.pageY = event.clientY +
-//             (doc && doc.scrollTop || body && body.scrollTop || 0) -
-//             (doc && doc.clientTop || body && body.clientTop || 0);
-//     }
-//     //not my function, https://stackoverflow.com/questions/7790725/javascript-track-mouse-position
+window.addEventListener('mousemove', (e) => {
+    rings.forEach(ring => {
+        ring.style.transform = `translateX(${e.clientX - 20}px) translateY(${e.clientY - 20}px)`;
+    });
+});
 
-//     moveCursor(event.pageX, event.pageY);
-// }
-// function moveCursor(pX, pY) {
-//     var diff = 10;
-//     if (!isInElement) {
-//         cursor.style.left = (pX - diff) + 'px';
-//         cursor.style.top = (pY - diff) + 'px';
-//         cursor.style.width = '30px';
-//         cursor.style.height = '30px';
-//         cursor.style.borderRadius = "50%";
-//     }
-// }
-// for (var i = 0; i < gElems.length; i++) {
-//     var e = gElems[i];
-//     e.addEventListener("mouseenter", enterCursor)
-//     e.addEventListener("mouseleave", leaveCursor)
-// }
-// function enterCursor(event) {
-//     isInElement = true;
-//     var elem = event.srcElement;
-//     var eX = elem.offsetLeft;
-//     var eY = elem.offsetTop;
-//     var eW = elem.offsetWidth;
-//     var eH = elem.offsetHeight;
-//     var diff = 6;
-//     cursor.style.left = eX - diff + 'px';
-//     cursor.style.top = eY - diff + 'px';
-//     cursor.style.width = (eW + diff * 2 - 1) + 'px';
-//     cursor.style.height = (eH + diff * 2 - 1) + 'px';
-//     cursor.style.borderRadius = "5px";
-// }
-// function leaveCursor(event) {
-//     isInElement = false;
-// }
+links.forEach(link => {
+    link.addEventListener('mouseenter', () => {
+        cursor.classList.add('cursor-zoom');
+    });
+    link.addEventListener('mouseleave', () => {
+        cursor.classList.remove('cursor-zoom');
+    });
+});
 // <!-- ======= MAGIC-CURSOR -END- ======= -->
 // <!-- ======= MAGIC-CURSOR -END- ======= -->
 // <!-- ======= MAGIC-CURSOR -END- ======= -->
