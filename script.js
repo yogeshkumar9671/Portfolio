16bbf24e01880076efad8c091a8bc6280f7c9ea1
yogeshkumar9671 <97506050+yogeshkumar9671@users.noreply.github.com>
2025-05-16 17:50:54 +0530

    Add files via upload
    Signed-off-by: yogeshkumar9671 <97506050+yogeshkumar9671@users.noreply.github.com>

--- script.js
+++ script.js
@@ -612,3 +612,43 @@ document.querySelectorAll('.has-animation').forEach(el => observer.observe(el));
 // <!-- ======= H2-H5-HEADING -END- ======= -->
 // <!-- ======= H2-H5-HEADING -END- ======= -->
 // <!-- ======= H2-H5-HEADING -END- ======= -->
+
+
+
+
+// <!-- ======= CONTACT-FORM START ======= -->
+// <!-- ======= CONTACT-FORM START ======= -->
+// <!-- ======= CONTACT-FORM START ======= -->
+var form = document.getElementById("my-form");
+
+async function handleSubmit(event) {
+    event.preventDefault();
+    var status = document.getElementById("my-form-status");
+    var data = new FormData(event.target);
+    fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(response => {
+        if (response.ok) {
+            status.innerHTML = "Thanks for your submission!";
+            form.reset()
+        } else {
+            response.json().then(data => {
+                if (Object.hasOwn(data, 'errors')) {
+                    status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+                } else {
+                    status.innerHTML = "Oops! There was a problem submitting your form"
+                }
+            })
+        }
+    }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form"
+    });
+}
+form.addEventListener("submit", handleSubmit)
+// <!-- ======= CONTACT-FORM -END- ======= -->
+// <!-- ======= CONTACT-FORM -END- ======= -->
+// <!-- ======= CONTACT-FORM -END- ======= -->
